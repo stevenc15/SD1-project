17037f7a4aad11c31ce959a8800096f7a8cc62a4
Steven <sc201789@ucf.edu>
2024-03-17 22:15:10 -0400

    added repo models folder to ml

---
+++ ml/repoModels/DBNwithpytorch/DBNwithpytorchArchitecture.py
@@ -0,0 +1,5 @@
+#Use model with access to DBN.py file and RBM.py
+#This is an example of how to access and train the DBN with pytorch: 
+layers = [7, 5, 2]
+dbn = DBN(10, layers)
+dbn.train_DBN(dataset)
