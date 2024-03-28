29e83253aeb8e4aaf53fdd1515099672787f776c
Steven <sc201789@ucf.edu>
2024-03-27 20:47:00 -0400

    Adjusted repo model files and added available requirements.txt files

--- ml/repoModels/DBNwithpytorch/DBNwithpytorchArchitecture.py
+++ ml/repoModels/DBNwithpytorch/DBNwithpytorchArchitecture.py
@@ -1,6 +1,12 @@
-#STILL CANT USE THIS MODEL, TQDM
+#STILL CANT USE THIS MODEL, TQDM not known
 #Use model with access to DBN.py file and RBM.py
 #This is an example of how to access and train the DBN with pytorch: 
+
+#define layers
 layers = [7, 5, 2]
+
+#method to initialize model
 dbn = DBN(10, layers)
+
+#method to train model
 dbn.train_DBN(dataset)
