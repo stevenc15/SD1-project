fc1c42bae6a64d09fc18889c1be92e2098f7b705
Carlos Arciniegas <ca273903@ucf.edu>
2024-06-24 22:36:29 -0400

    Added next single imu point, imu loader, and deleted unnecessary files

--- Flask/app.py
+++ Flask/app.py
@@ -14,6 +14,7 @@ sample_rate_joint = None
 data_imu = None
 sample_rate_imu = None
 current_joint_index = 0
+current_imu_index = 0
 
 def load_joint_data():
 
@@ -25,8 +26,19 @@ def load_joint_data():
     
     data_joint, sample_rate_joint = sf.read(path)
 
+def load_imu_data():
+
+    global data_imu, sample_rate_imu 
+    # Change file path accordingly
+    path = 'imu_data.wav'
+    if not os.path.exists(path):
+            return jsonify({'error': 'File not found with this path'}), 404
+    
+    data_imu, sample_rate_imu = sf.read(path)
+
 # Loading joint data
 load_joint_data()    
+load_imu_data()
 
 @app.route('/get_joint_data', methods=['GET'])
 def get_joint_data():
@@ -38,10 +50,13 @@ def get_joint_data():
             'sample_rate': sample_rate_joint,
             'data_joint': data_list 
         }
-        json_str = json.dumps(response)
+
         json_obj = jsonify(response)
-        with open('joint_data.json', 'w') as file:
-            file.write(json_str)
+        
+        # mock data for tyler
+        # json_str = json.dumps(response)
+        # with open('joint_data.json', 'w') as file:
+        #     file.write(json_str)
 
         return json_obj
     
@@ -67,16 +82,10 @@ def get_next_joint_point():
 @app.route('/get_imu_data', methods=['GET'])
 def get_imu_data():
     try:
-        # Change file path accordingly
-        path = 'imu_data.wav'
-
-        if not os.path.exists(path):
-            return jsonify({'error': 'File not found with this path'}), 404
-
-        data_imu, sample_rate_imu = sf.read(path)
-
         # Necessary because numpy arrays are not serializable
         data_list = data_imu.tolist()
+        data_list_length = len(data_list)
+        print(data_list_length)
 
         response = {
             'sample_rate': sample_rate_imu,
@@ -87,6 +96,22 @@ def get_imu_data():
     
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+# Returns a single point and increments global index to get ready for the next point
+@app.route('/get_next_imu_point', methods=['GET'])
+def get_next_imu_point():
+
+    global current_imu_index
+    if current_imu_index >= len(data_imu):
+        return jsonify({"error": "All data has been returned"}), 404
+    
+    next_point = data_imu[current_imu_index]
+    response = {
+        'index': current_imu_index,
+        'point': next_point
+    }
+    current_imu_index += 1
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(port=5000)
