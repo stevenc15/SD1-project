0a9f1b543ccdc8a6b1ceaa9d9223014662d524d6
carlos <carciniegas@knights.ucf.edu>
2024-06-29 23:53:48 -0400

    Added library that allows to interact with the API

--- Flask/app.py
+++ Flask/app.py
@@ -2,8 +2,11 @@ from flask import Flask, jsonify
 import soundfile as sf
 import os
 import json
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route("/")
 def index():
@@ -114,4 +117,4 @@ def get_next_imu_point():
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
