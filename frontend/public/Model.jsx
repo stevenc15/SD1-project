485ebab96a7fdd8f36b2889810e35908c13697ea
tchalfpenny <tchalfpenny@gmail.com>
2024-07-23 17:45:25 -0400

    download fixes and sensor toggle

--- frontend/public/Model.jsx
+++ frontend/public/Model.jsx
@@ -16,12 +16,13 @@ export default function Model(props) {
   const { actions } = useAnimations(animations, group)
 
   const [apiData, setData] = useState({
-    values: [],
+    timeData: [],
+    angleData: [],
     dataLength: 0,
     timeElapsed: 0,
   })
 
-  console.log(apiData)
+  //console.log(apiData)
 
   useEffect(() => {
     fetch("http://localhost:5000/joint_angle_pred", {
@@ -32,24 +33,32 @@ export default function Model(props) {
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log(data)
-        console.log(data.length)
-        console.log(data[0])
-        console.log(data[3074])
+
+        // console.log(data)
+        // console.log(data.length)
+        // console.log(data[0])
+        // console.log(data[3074])
+
+        const arrayColumn = (arr, n) => arr.map(x => x[n]);
+        let tmpLen = data.length
+
         setData({
-          // values: data.data_joint,
+          timeData: arrayColumn(data, 0),
+          angleData: arrayColumn(data, 1),
+          dataLength: tmpLen,
+          timeElapsed: (data[tmpLen-1][0] - data[0][0])
         })
+        console.log(apiData.timeElapsed + " of data loaded to Model.")
+        //group.rotation.y = group.rotation.y + (90 * Math.PI / 180)
       });
   }, []);
 
-  // console.log(apiData.values)
-  // let dataLength = (apiData.values).length
-  // console.log(dataLength)
+  //console.log(apiData.angleData)
 
   const statObject = {
     xRot: 0,
     yRot: 0,
-    zRot: 0,
+    zRot: 180,
     armAng: 0,
     Framerate: 60,
     Interpolate: false,
@@ -61,26 +70,26 @@ export default function Model(props) {
   let interval = 1 / statObject.Framerate
   let currentFrame = 0
 
-  //console.log(group)
-  //console.log(forearm)
+  // console.log(group)
+  // console.log(forearm)
 
   useEffect(() => {
     const gui = new GUI()
-    gui.add(statObject, 'xRot', 0, 360, 2)
-    gui.add(statObject, 'yRot', 0, 360, 2)
-    gui.add(statObject, 'zRot', 0, 360, 2)
+    gui.add(statObject, 'xRot', 0, 360, 1)
+    gui.add(statObject, 'yRot', 0, 360, 1)
+    gui.add(statObject, 'zRot', 0, 360, 1)
     //gui.add(forearm.current.skeleton.bones[2].rotation, 'x', Math.PI *(3/2), Math.PI * 2)
     gui.add(statObject, 'armAng', 0, 180)
     gui.add(statObject, 'Framerate', 24, 120, 1)
     gui.add(statObject, 'Interpolate')
     gui.add(statObject, 'PausePlay')
     gui.onChange(event => {
+      console.log(event)
       group.current.rotation.x = statObject.xRot * (Math.PI / 180)
       group.current.rotation.y = statObject.yRot * (Math.PI / 180)
       group.current.rotation.z = statObject.zRot * (Math.PI / 180)
-      forearm.current.skeleton.bones[2].rotation.x = (statObject.armAng - 90) * (Math.PI / 180)
+      //forearm.current.skeleton.bones[2].rotation.x = (statObject.armAng - 90) * (Math.PI / 180)
       interval = 1 / statObject.Framerate
-      console.log(apiData.values)
     })
     return () => {
       gui.destroy()
@@ -95,33 +104,43 @@ export default function Model(props) {
       if (delta > interval) {
         if (statObject.PausePlay == true)
           {
+            //console.log(currentFrame)
             currentFrame = currentFrame + 1
+            //props.passCurrentFrame(currentFrame)
           }
         //console.log(currentFrame)
         //console.log(statObject.PausePlay)
-        //forearm.current.skeleton.bones[2].rotation.x = (apiData.values[currentFrame]) * (Math.PI / 180)
+        forearm.current.skeleton.bones[2].rotation.x = (apiData.angleData[currentFrame] - 180) * (Math.PI / 180)
+        //console.log(apiData.angleData[currentFrame] - 180)
       }
       delta = delta % interval
-      // if (currentFrame > dataLength) {
-      //   currentFrame = 0
-      // }
+      if (currentFrame > apiData.dataLength) {
+        currentFrame = 0
+      }
     }
   })
+
+  // props.passChildData1(apiData.angleData)
+  // props.passChildData2(apiData.timeData)
 
   // function rotLeft()
   // {
   //   group.rotate =- 90
   // }
-
   // function rotRight()
   // {
   //   group.rotate =+ 90
   // }
-
   // function resetCam()
   // {
   //   group.rotate = 0
   // }
+
+  useEffect(() => {
+    props.passChildData1(apiData.angleData)
+    props.passChildData2(apiData.timeData)
+    }, [apiData]);
+
 
   return (
     <>
--- frontend/src/scenes/predictions/predictions.jsx
+++ frontend/src/scenes/predictions/predictions.jsx
@@ -1,14 +1,29 @@
 import { Canvas } from '@react-three/fiber'
+import { useState } from 'react'
+import Select from 'react-select'
 import Model from '/public/Model'
 
 function Predictions() {
 
+  const [childData1, setChildData1] = useState([]);
+  const [childData2, setChildData2] = useState([]);
+  const [recStatus, setRecStatus] = useState("0")
+  const [recStatusMessage, setRecStatMsg] = useState("Start Recording")
+  const [frame1, setFrame1] = useState(0)
+  const [frame2, setFrame2] = useState(0)
+  const [curFrame, setCurrentFrame] = useState("")
+  const [sensModel, setSensModel] = useState("6sensor")
+  //console.log(childData1)
+  //console.log(childData2)
+
+  const options = [
+    { value: '1sensor', label: '1 Sensor Model' },
+    { value: '6sensor', label: '6 Sensor Model' }
+  ]
+
   const jsonFileDownload = () => {
-    const json_data = {
-      name: "Dedar",
-      age: "14",
-      address: "House #28",
-    };
+    const yourArray = childData1
+    let json_data = JSON.stringify(yourArray);
     const fileName = "joint_data.json";
     const data = new Blob([JSON.stringify(json_data)], { type: "text/json" });
     const jsonURL = window.URL.createObjectURL(data);
@@ -20,16 +35,58 @@ function Predictions() {
     document.body.removeChild(link);
   };
 
+  const jsonFileDownloadRec = () => {
+    const yourArray2 = childData1.slice(frame1, frame2)
+    console.log(yourArray2)
+    let json_data = JSON.stringify(yourArray2);
+    const fileName = "joint_data.json";
+    const data = new Blob([JSON.stringify(json_data)], { type: "text/json" });
+    const jsonURL = window.URL.createObjectURL(data);
+    const link = document.createElement("a");
+    document.body.appendChild(link);
+    link.href = jsonURL;
+    link.setAttribute("download", fileName);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  const updateRecStatus = () => {
+    if(recStatus % 3 == 0){
+      console.log("Recording started.")
+      setRecStatus(1)
+      setRecStatMsg("Stop Recording")
+      setFrame1(curFrame)
+      console.log(frame1)
+    }
+    if(recStatus % 3 == 1){
+      console.log("Recording ended.")
+      setRecStatus(2)
+      setRecStatMsg("Download Recorded Data")
+      setFrame2(curFrame)
+      console.log(frame2)
+    }
+    if(recStatus % 3 == 2){
+      console.log("Download recording.")
+      setRecStatus(0)
+      setRecStatMsg("Start Recording")
+      jsonFileDownloadRec()
+    }
+  }
+
   return (
     <>
+      <Select 
+        defaultValue={sensModel}
+        options={options}
+        />
       <Canvas>
-        <Model />
+        <Model passChildData1={setChildData1} passChildData2={setChildData2} passCurrentFrame={setCurrentFrame}/>
       </Canvas>
       <button type="button">Rotate Left</button>
       <button type="button">Reset Camera</button>
       <button type="button">Rotate Right</button>
-      <button type="button">Start/Stop Recording</button>
-      <button type="button" onClick={jsonFileDownload}>Download Recording</button>
+      <button type="button" onClick={updateRecStatus}>{recStatusMessage}</button>
+      <button type="button" onClick={jsonFileDownload}>Download Full Recording</button>
     </>
   )
 }
