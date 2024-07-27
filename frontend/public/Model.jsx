321f9f6ffbb524b40d68b9ebe7c72485cb73534e
tchalfpenny <135466340+tchalfpenny@users.noreply.github.com>
2024-07-27 19:32:32 -0400

    Update Model.jsx

--- frontend/public/Model.jsx
+++ frontend/public/Model.jsx
@@ -1,127 +1,134 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { useGLTF, useAnimations, Gltf } from '@react-three/drei'
-import { useFrame } from '@react-three/fiber'
-import { Suspense } from 'react'
-import { OrbitControls } from '@react-three/drei'
-import * as THREE from 'three'
-import { GUI } from 'lil-gui'
-//import data from '/public/joint_data'
+import React, { useEffect, useRef, useState } from "react";
+import { useGLTF, useAnimations, Gltf } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { Suspense } from "react";
+import { OrbitControls } from "@react-three/drei";
+import * as THREE from "three";
+import { GUI } from "lil-gui";
 
 export default function Model(props) {
-  //const dataLen = data.numbers.length
-  //console.log(dataLen)
-  const group = useRef()
-  const forearm = useRef()
-  const { nodes, materials, animations } = useGLTF('/arm.gltf')
-  const { actions } = useAnimations(animations, group)
+  const group = useRef();
+  const forearm = useRef();
+  const { nodes, materials, animations } = useGLTF("/arm.gltf");
+  const { actions } = useAnimations(animations, group);
+  const modelPlaying = useRef(false);
+  console.log(props.selectedModel);
 
   const [apiData, setData] = useState({
     timeData: [],
     angleData: [],
     dataLength: 0,
     timeElapsed: 0,
-  })
-
-  //console.log(apiData)
+  });
 
   useEffect(() => {
-    fetch("http://localhost:5000/joint_angle_pred", {
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      }
-    })
-      .then((res) => res.json())
-      .then((data) => {
+    if (props.selectedModel === 1) {
+      console.log("1 Sensor Output.");
+      fetch("http://localhost:5000/joint_angle_pred", {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
 
-        // console.log(data)
-        // console.log(data.length)
-        // console.log(data[0])
-        // console.log(data[3074])
+          const arrayColumn = (arr, n) => arr.map((x) => x[n]);
+          let tmpLen = data.length;
 
-        const arrayColumn = (arr, n) => arr.map(x => x[n]);
-        let tmpLen = data.length
+          setData({
+            timeData: arrayColumn(data, 0),
+            angleData: arrayColumn(data, 1),
+            dataLength: tmpLen,
+            timeElapsed: data[tmpLen - 1][0] - data[0][0],
+          });
+        });
+    } else if (props.selectedModel === 6) {
+      console.log("6 Sensor Output.");
+      fetch("http://localhost:5000/joint_angle_pred", {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
 
-        setData({
-          timeData: arrayColumn(data, 0),
-          angleData: arrayColumn(data, 1),
-          dataLength: tmpLen,
-          timeElapsed: (data[tmpLen-1][0] - data[0][0])
-        })
-        console.log(apiData.timeElapsed + " of data loaded to Model.")
-        //group.rotation.y = group.rotation.y + (90 * Math.PI / 180)
-      });
+          const arrayColumn = (arr, n) => arr.map((x) => x[n]);
+          let tmpLen = data.length;
+
+          setData({
+            timeData: arrayColumn(data, 0),
+            angleData: arrayColumn(data, 1),
+            dataLength: tmpLen,
+            timeElapsed: data[tmpLen - 1][0] - data[0][0],
+          });
+        });
+    }
   }, []);
 
-  //console.log(apiData.angleData)
+  console.log(apiData.timeElapsed + " of data loaded to Model.");
 
   const statObject = {
-    xRot: 0,
-    yRot: 0,
-    zRot: 180,
-    armAng: 0,
+    xRot: 270,
+    yRot: 180,
+    zRot: 90,
     Framerate: 60,
     Interpolate: false,
-    PausePlay: true,
-  }
+    isPlaying: true,
+  };
 
-  let clock = new THREE.Clock()
-  let delta = 0
-  let interval = 1 / statObject.Framerate
-  let currentFrame = 0
+  let clock = new THREE.Clock();
+  let delta = 0;
+  let interval = 1 / statObject.Framerate;
 
   // console.log(group)
   // console.log(forearm)
 
   useEffect(() => {
-    const gui = new GUI()
-    gui.add(statObject, 'xRot', 0, 360, 1)
-    gui.add(statObject, 'yRot', 0, 360, 1)
-    gui.add(statObject, 'zRot', 0, 360, 1)
-    //gui.add(forearm.current.skeleton.bones[2].rotation, 'x', Math.PI *(3/2), Math.PI * 2)
-    gui.add(statObject, 'armAng', 0, 180)
-    gui.add(statObject, 'Framerate', 24, 120, 1)
-    gui.add(statObject, 'Interpolate')
-    gui.add(statObject, 'PausePlay')
-    gui.onChange(event => {
-      console.log(event)
-      group.current.rotation.x = statObject.xRot * (Math.PI / 180)
-      group.current.rotation.y = statObject.yRot * (Math.PI / 180)
-      group.current.rotation.z = statObject.zRot * (Math.PI / 180)
-      //forearm.current.skeleton.bones[2].rotation.x = (statObject.armAng - 90) * (Math.PI / 180)
-      interval = 1 / statObject.Framerate
-    })
+    group.current.rotation.x = 270 * (Math.PI / 180);
+    group.current.rotation.y = 180 * (Math.PI / 180);
+    group.current.rotation.z = 90 * (Math.PI / 180);
+    const gui = new GUI();
+    gui.add(statObject, "xRot", 0, 360, 1);
+    gui.add(statObject, "yRot", 0, 360, 1);
+    gui.add(statObject, "zRot", 0, 360, 1);
+    gui.add(statObject, "Framerate", 24, 120, 1);
+    gui.add(statObject, "Interpolate");
+    gui.add(statObject, "isPlaying");
+    gui.onChange((event) => {
+      console.log(event);
+      group.current.rotation.x = statObject.xRot * (Math.PI / 180);
+      group.current.rotation.y = statObject.yRot * (Math.PI / 180);
+      group.current.rotation.z = statObject.zRot * (Math.PI / 180);
+      interval = 1 / statObject.Framerate;
+      console.log(statObject.isPlaying);
+      console.log(typeof statObject.isPlaying);
+      modelPlaying.current = statObject.isPlaying;
+    });
     return () => {
-      gui.destroy()
-    }
-  }, [])
+      gui.destroy();
+    };
+  }, []);
 
   //onfinishchange
 
   useFrame(() => {
-    if (statObject.Interpolate == false) {
-      delta += clock.getDelta();
-      if (delta > interval) {
-        if (statObject.PausePlay == true)
-          {
-            //console.log(currentFrame)
-            currentFrame = currentFrame + 1
-            //props.passCurrentFrame(currentFrame)
-          }
-        //console.log(currentFrame)
-        //console.log(statObject.PausePlay)
-        forearm.current.skeleton.bones[2].rotation.x = (apiData.angleData[currentFrame] - 180) * (Math.PI / 180)
-        //console.log(apiData.angleData[currentFrame] - 180)
+    delta += clock.getDelta();
+    if (delta > interval) {
+      if (props.isPlaying.current) {
+        props.currentFrame.current = props.currentFrame.current + 1;
+        if ((props.currentFrame.current == apiData.dataLength - 1)) {
+          props.currentFrame.current = 0;
+        }
       }
-      delta = delta % interval
-      if (currentFrame > apiData.dataLength) {
-        currentFrame = 0
-      }
+      forearm.current.skeleton.bones[2].rotation.x =
+        apiData.angleData[props.currentFrame.current] * (Math.PI / 180);
     }
-  })
-
-  // props.passChildData1(apiData.angleData)
-  // props.passChildData2(apiData.timeData)
+    delta = delta % interval;
+  });
 
   // function rotLeft()
   // {
@@ -137,10 +144,9 @@ export default function Model(props) {
   // }
 
   useEffect(() => {
-    props.passChildData1(apiData.angleData)
-    props.passChildData2(apiData.timeData)
-    }, [apiData]);
-
+    props.passAngleData(apiData.angleData);
+    props.passTimeData(apiData.timeData);
+  }, [apiData]);
 
   return (
     <>
@@ -152,11 +158,21 @@ export default function Model(props) {
             <group name="Sketchfab_model" rotation={[-Math.PI / 2, 0, 0]}>
               <group name="root">
                 <group name="GLTF_SceneRootNode" rotation={[Math.PI / 2, 0, 0]}>
-                  <group name="Armature_73" position={[-0.123, -0.111, -1.771]} rotation={[1.583, 0, 0]}>
+                  <group
+                    name="Armature_73"
+                    position={[-0.123, -0.111, -1.771]}
+                    rotation={[1.583, 0, 0]}
+                  >
                     <group name="GLTF_created_0">
                       <primitive object={nodes.GLTF_created_0_rootJoint} />
                       <group name="Cube017_72" />
-                      <skinnedMesh ref={forearm} name="Object_7" geometry={nodes.Object_7.geometry} material={materials.Cloth} skeleton={nodes.Object_7.skeleton} />
+                      <skinnedMesh
+                        ref={forearm}
+                        name="Object_7"
+                        geometry={nodes.Object_7.geometry}
+                        material={materials.Cloth}
+                        skeleton={nodes.Object_7.skeleton}
+                      />
                     </group>
                   </group>
                 </group>
@@ -166,7 +182,7 @@ export default function Model(props) {
         </group>
       </Suspense>
     </>
-  )
+  );
 }
 
-useGLTF.preload('/arm.gltf')
+useGLTF.preload("/arm.gltf");
