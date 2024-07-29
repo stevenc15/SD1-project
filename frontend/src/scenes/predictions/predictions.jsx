73284229961a0baa0a1b81e58eec27d5adb57150
tchalfpenny <135466340+tchalfpenny@users.noreply.github.com>
2024-07-29 02:25:04 -0400

    Update predictions.jsx

--- frontend/src/scenes/predictions/predictions.jsx
+++ frontend/src/scenes/predictions/predictions.jsx
@@ -2,12 +2,38 @@ import { Canvas } from "@react-three/fiber";
 import { useState, useRef } from "react";
 import Select from "react-select";
 import Model from "/public/Model";
-import { Box } from "@mui/material";
+import DashboardBox from "@/components/DashboardBox";
+import { useTheme } from "@mui/material";
+import {
+  Box,
+  Button,
+  Alert,
+  AlertTitle,
+  IconButton,
+  Collapse,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 import demo from "/public/IMG_2850.mov";
+import FlexBetween from "@/components/FlexBetween";
+
+// import Alert from "@mui/material/Alert";
+// import AlertTitle from "@mui/material/AlertTitle";
 
 function Predictions() {
+  const palette = useTheme();
+  console.log(palette);
+
+  const gridTemplateLargeScreens = `
+    "e e e"
+    "a b c"
+    "d d d"
+`;
+
+  const tempData = 0;
   const [angleData, setAngleData] = useState([]);
   const [timeData, setTimeData] = useState([]);
+
+  const [open, setOpen] = useState(true);
 
   const isPlaying = useRef(true);
   const setPlay = () => {
@@ -21,7 +47,7 @@ function Predictions() {
   const frame2 = useRef(0);
 
   const setRecStatus = () => {
-    console.log(recStatus.current)
+    console.log(recStatus.current);
     if (recStatus.current == 0) {
       frame1.current = currentFrame.current;
       recStatus.current = recStatus.current + 1;
@@ -33,9 +59,6 @@ function Predictions() {
       jsonFileDownload();
     }
   };
-
-  // const [recStatus, setRecStatus] = useState("0")
-  // const [recStatusMessage, setRecStatMsg] = useState("Start Recording")
 
   const options = [
     { value: 1, label: "1 Sensor Model" },
@@ -82,52 +105,100 @@ function Predictions() {
 
   return (
     <>
-      <Select
-        defaultValue={options[0]}
-        onChange={setSelectedModel}
-        options={options}
-      />
-      <Box
-        height={500}
-        width={1000}
-        my={4}
-        display="flex"
-        alignItems="center"
-        gap={4}
-        p={2}
-        sx={{ border: "2px solid grey" }}
-      >
-        <Canvas>
-          <Model
-            passAngleData={setAngleData}
-            passTimeData={setTimeData}
-            selectedModel={selectedModel}
-            isPlaying={isPlaying}
-            currentFrame={currentFrame}
-            rotDir={rotDir}
-          />
-        </Canvas>
-        <video width="500" height="500" loop={true} autoPlay controls>
-          <source src={demo} type="video/mp4" />
-        </video>
-      </Box>
-      <button type="button" onClick={setRotLeft}>
-        Rotate Left
-      </button>
-      <button type="button">Reset Camera</button>
-      <button type="button" onClick={setRotRight}>
-        Rotate Right
-      </button>
-      {/* <button type="button" onClick={updateRecStatus}>{recStatusMessage}</button> */}
-      <button type="button" onClick={setRecStatus}>
-        Start/Stop/Download Rec
-      </button>
-      <button type="button" onClick={jsonFileDownload}>
-        Download Full Recording
-      </button>
-      <button type="button" onClick={setPlay}>
-        Pause/Play
-      </button>
+      <Collapse in={open}>
+        <Alert
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setOpen(false);
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+          sx={{ mb: 2 }}
+        >
+          {tempData} seconds of input data loaded to model.
+        </Alert>
+      </Collapse>
+      <div>
+        <Box
+          width="100%"
+          height="100%"
+          display="grid"
+          gap="1.5rem"
+          sx={{
+            // modify the minmax for the size of the graphs
+            // fr -> fractional units (split each box evenly)
+            gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+            gridTemplateRows: "repeat(1, minmax(90px, 1fr))",
+            gridTemplateAreas: gridTemplateLargeScreens,
+          }}
+        >
+          <Box gridArea="e">
+            <Select 
+              defaultValue={options[0]}
+              onChange={setSelectedModel}
+              options={options}
+            />
+          </Box>
+          <DashboardBox gridArea="a">
+            <Canvas id="modelCanvas">
+              <Model
+                passAngleData={setAngleData}
+                passTimeData={setTimeData}
+                selectedModel={selectedModel}
+                isPlaying={isPlaying}
+                currentFrame={currentFrame}
+                rotDir={rotDir}
+              />
+            </Canvas>
+          </DashboardBox>
+          <DashboardBox gridArea="b">
+            <video width="300" height="400" loop={true} autoPlay controls>
+              <source src={demo} type="video/mp4" />
+            </video>
+          </DashboardBox>
+          <DashboardBox gridArea="c">
+            <video width="300" height="400" loop={true} autoPlay controls>
+              <source src={demo} type="video/mp4" />
+            </video>
+          </DashboardBox>
+          <DashboardBox
+            gridArea="d"
+            sx={{
+              display: "flex",
+              justifyContent: "space-evenly",
+            }}
+          >
+            <Button variant="contained" type="button" onClick={setRotLeft}>
+              Rotate Left
+            </Button>
+            <Button variant="contained" type="button" onClick={setPlay}>
+              Pause/Play
+            </Button>
+            <Button variant="contained" type="button" onClick={setRotRight}>
+              Rotate Right
+            </Button>
+            <Button variant="contained" type="button">
+              Reset Camera
+            </Button>
+            <Button variant="contained" type="button" onClick={setRecStatus}>
+              Start/Stop/Download Rec
+            </Button>
+            <Button
+              variant="contained"
+              type="button"
+              onClick={jsonFileDownload}
+            >
+              Download Full Recording
+            </Button>
+          </DashboardBox>
+        </Box>
+      </div>
     </>
   );
 }
